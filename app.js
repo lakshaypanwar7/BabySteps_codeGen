f92b84a5bc67609072a87a34857b4b4f78293732
lakshaypanwar7 <122521326+lakshaypanwar7@users.noreply.github.com>
2024-06-29 16:30:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
